bd2d9499222fdd4dd06388ad13e72cdb7432417a
ankit-raj78 <ankit.raj78800@gmail.com>
2025-02-02 19:20:17 -0500

    init

---
+++ my_malloc.h
@@ -0,0 +1,17 @@
+
+#ifndef __MY_MALLOC_H__
+#define __MY_MALLOC_H__
+
+#include <stddef.h>
+
+void* ff_malloc(size_t size);
+void ff_free(void* ptr);
+
+void* bf_malloc(size_t size);
+void bf_free(void* ptr);
+
+// Performance measurement functions
+unsigned long get_data_segment_size(); 
+unsigned long get_data_segment_free_space_size();
+
+#endif
